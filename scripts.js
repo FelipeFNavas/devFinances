c11c0bd7335a06024b49302b2eefaf6017bf4fdf
Felipe Navas <felipefnavas@gmail.com>
2021-11-04 23:54:18 -0300

    feat: Salvando dados no localStorage - termino aula3 3

--- scripts.js
+++ scripts.js
@@ -11,24 +11,18 @@ const Modal = {
     }
 }
 
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem('dev.finances:transaction')) || [];
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.finances:transaction", JSON.stringify(transactions));
+    }
+}
+
 const Transaction = {
-    all: [
-        {
-            description: 'luz',
-            amount: -50000,
-            date: '23/01/2021',
-        },
-        {
-            description: 'website',
-            amount: 500000,
-            date: '23/01/2021',
-        },
-        {
-            description: 'internet',
-            amount: -20000,
-            date: '23/01/2021',
-        }
-    ],
+    all: Storage.get(),
 
     add(transaction){
         Transaction.all.push(transaction);
@@ -78,11 +72,12 @@ const DOM = {
 
     addTransaction(transaction, index){
         const tr = document.createElement('tr');
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+        tr.dataset.index = index;
 
         DOM.transactionsContainer.appendChild(tr);
     },
-    innerHTMLTransaction(transaction){
+    innerHTMLTransaction(transaction, index){
         const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
         const amount = Utils.formatCurrency(transaction.amount);
@@ -92,7 +87,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover trasação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
         `
 
@@ -169,7 +164,6 @@ const Form = {
         }
 
 
-
     },
 
     formatValues(){
@@ -182,14 +176,21 @@ const Form = {
         return {description, amount, date};
     },
 
+    clearFields(){
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
 
     submit(event){
         event.preventDefault();
         try{
-    
-            //Form.validateFilds();
-    
-            Form.formatValues();
+            Form.validateFilds();
+            const transaction = Form.formatValues()
+            Transaction.add(transaction)
+            Form.clearFields()
+            Modal.close()
 
         }catch(error){
             alert(error.message);
@@ -198,16 +199,16 @@ const Form = {
     }
 }
 
-
 const App = {
     init(){
         
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index)
         })
         
         DOM.updateBalance();
         
+        Storage.set(Transaction.all);
     },
     reload(){ 
         DOM.clearTransactions();
